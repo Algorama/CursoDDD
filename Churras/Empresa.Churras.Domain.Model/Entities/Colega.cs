9daf14ce64a0750bb6dced9331ad5b8910408033
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-08-12 15:15:42 -0300

    Modelo de Entidades Inicial

---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Colega.cs
@@ -0,0 +1,10 @@
+﻿using Kernel.Domain.Model.Entities;
+
+namespace Empresa.Churras.Domain.Model.Entities
+{
+    public class Colega : EntityKeySeq
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -0,0 +1,15 @@
+﻿using Kernel.Domain.Model.Entities;
+using System;
+
+namespace Empresa.Churras.Domain.Model.Entities
+{
+    public class Evento : EntityKeySeq
+    {
+        public string DonoDaCasa { get; set; }
+        public string TipoEvento { get; set; }
+        public DateTime Dia { get; set; }
+        public string Periodo { get; set; }
+        public string ColegasQueConfirmaramPresenca { get; set; }
+        public string OQueCadaColegaVaiLevar { get; set; }
+    }
+}
