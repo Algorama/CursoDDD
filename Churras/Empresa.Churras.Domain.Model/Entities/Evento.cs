599676c0f5e9a94c60d6efa6da36c4690146220a
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-08-13 09:34:09 -0300

    Definição das Associações do Modelo

--- Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -1,15 +1,16 @@
 ﻿using Kernel.Domain.Model.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Empresa.Churras.Domain.Model.Entities
 {
     public class Evento : EntityKeySeq
     {
-        public string DonoDaCasa { get; set; }
+        public long DonoDaCasaKey { get; set; }
+        public Colega DonoDaCasa { get; set; }
         public string TipoEvento { get; set; }
         public DateTime Dia { get; set; }
         public string Periodo { get; set; }
-        public string ColegasQueConfirmaramPresenca { get; set; }
-        public string OQueCadaColegaVaiLevar { get; set; }
+        public IList<EventoColegaConfirmado> ColegasConfirmados { get; set; }
     }
 }
---
+++ Churras/Empresa.Churras.Domain.Model/Entities/EventoColegaConfirmado.cs
@@ -0,0 +1,9 @@
+﻿namespace Empresa.Churras.Domain.Model.Entities
+{
+    public class EventoColegaConfirmado
+    {
+        public long ColegaKey { get; set; }
+        public string ColegaNome { get; set; }
+        public string OQueVaiLevar { get; set; }
+    }
+}
