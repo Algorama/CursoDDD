ad534dc1b2322f5e0ed5dcd8dd288168e0ba0d4e
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-08-13 12:30:46 -0300

    Implementação dos Métodos de Comportamento na Entidade Evento

--- Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -3,16 +3,44 @@ using Empresa.Churras.Domain.Model.ValueObjects;
 using Kernel.Domain.Model.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Empresa.Churras.Domain.Model.Entities
 {
     public class Evento : EntityKeySeq
     {
+        public string Nome { get; set; }
         public long DonoDaCasaKey { get; set; }
         public Colega DonoDaCasa { get; set; }
         public TipoEvento Tipo { get; set; }
         public DateTime Dia { get; set; }
         public Periodo Periodo { get; set; }
         public IList<EventoColegaConfirmado> ColegasConfirmados { get; set; }
+
+        public Evento()
+        {
+            ColegasConfirmados = new List<EventoColegaConfirmado>();
+        }
+
+        public void ConfirmarPresenca(Colega colega)
+        {
+            if(ColegasConfirmados == null)
+                ColegasConfirmados = new List<EventoColegaConfirmado>();
+
+            ColegasConfirmados.Add(new EventoColegaConfirmado 
+            { 
+                ColegaKey = colega.Key,
+                ColegaNome = colega.Nome 
+            });
+        }
+
+        public void CancelarPresenca(Colega colega)
+        {
+            if (ColegasConfirmados == null) return;
+
+            var confirmacao = ColegasConfirmados.FirstOrDefault(x => x.ColegaKey == colega.Key);
+            if (confirmacao != null)
+                ColegasConfirmados.Remove(confirmacao);
+        }
     }
 }
---
+++ Churras/Empresa.Churras.Tests/Entities/EventoTest.cs
@@ -0,0 +1,48 @@
+﻿using Empresa.Churras.Domain.Model.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Empresa.Churras.Tests.Entities
+{
+    [TestClass]
+    public class EventoTest
+    {
+
+        [TestMethod]
+        public void ConfirmarPresenca_Test()
+        {
+            var colega = new Colega
+            {
+                Key = 1,
+                Nome = "Tião"
+            };
+            var evento = new Evento { Nome = "Churras na Casa do Ary" };
+
+            evento.ConfirmarPresenca(colega);
+
+            var confirmacao = evento.ColegasConfirmados.FirstOrDefault(x => x.ColegaNome == "Tião");
+
+            Assert.IsNotNull(confirmacao, "Não encontrou o Tião na Lista de Confirmação");
+            Assert.AreEqual(colega.Key, confirmacao.ColegaKey, 
+                $"Confirmação não é do Colega com o Id: {colega.Key}");
+        }
+
+        [TestMethod]
+        public void CancelarPresenca_Test()
+        {
+            var colega = new Colega
+            {
+                Key = 1,
+                Nome = "Tião"
+            };
+            var evento = new Evento { Nome = "Churras na Casa do Ary" };
+
+            evento.ConfirmarPresenca(colega);
+            evento.CancelarPresenca(colega);
+
+            var confirmacao = evento.ColegasConfirmados.FirstOrDefault(x => x.ColegaNome == "Tião");
+
+            Assert.IsNull(confirmacao, "O Tião continua na lista de Presença!");
+        }
+    }
+}
