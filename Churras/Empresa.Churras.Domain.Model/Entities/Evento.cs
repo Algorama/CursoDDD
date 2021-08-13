892a7c45c53202c2b1cb3a69aa7bee7d275a5975
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-08-13 11:48:09 -0300

    Criado o Enum TipoEvento

--- Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -1,4 +1,5 @@
-﻿using Empresa.Churras.Domain.Model.ValueObjects;
+﻿using Empresa.Churras.Domain.Model.Enums;
+using Empresa.Churras.Domain.Model.ValueObjects;
 using Kernel.Domain.Model.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ namespace Empresa.Churras.Domain.Model.Entities
     {
         public long DonoDaCasaKey { get; set; }
         public Colega DonoDaCasa { get; set; }
-        public string TipoEvento { get; set; }
+        public TipoEvento Tipo { get; set; }
         public DateTime Dia { get; set; }
         public Periodo Periodo { get; set; }
         public IList<EventoColegaConfirmado> ColegasConfirmados { get; set; }
---
+++ Churras/Empresa.Churras.Domain.Model/Enums/TipoEvento.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Empresa.Churras.Domain.Model.Enums
+{
+    public enum TipoEvento
+    {
+        [Description("Churrasco")]
+        Churras = 0,
+
+        Pizza = 1,
+
+        Lanche = 2,
+
+        Outros = 999
+    }
+}
